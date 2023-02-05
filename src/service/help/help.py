d09cc8651a28dac07641686af4e5d4b9e9bd41a2
Noob Science <noobscience123@gmail.com>
2023-02-05 15:54:12 +0530

    📝 Improve

--- src/service/help/help.py
+++ src/service/help/help.py
@@ -10,8 +10,8 @@ class Help:
     def help_content(self):
         from pyfiglet import Figlet
         figlet = Figlet(font="doom")
-        console.print(figlet.renderText("PEEP"), style="green")
-        console.print(Panel.fit("NoobScience's PEEP: Personalized Environment and Execution Prompt", title="Help", border_style="green"), justify="center")
+        console.print(figlet.renderText("PEEP"), style="green", justify="center")
+        console.print(Panel.fit(f"{os.getlogin().capitalize()}'s PEEP: Personalized Environment and Execution Prompt", title="Help", border_style="green"), justify="center")
         console.print("PEEP is the friendly assistant we have all been looking for. "
                       "PEEP lives in the command line and loves it there. And it's here to help you with"
                       " your daily tasks. PEEP is not perfect, but it's getting there. PEEP is a work in progress."
