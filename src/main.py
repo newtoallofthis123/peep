ec33680c3b5dc4070198e81d31f43c6aa573a39c
Noob Science <noobscience123@gmail.com>
2023-02-10 14:08:28 +0530

    🔧 Improve help

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from service.execute import *
 from service.brain import *
 
-
 def main():
     from rich.traceback import install
     install(show_locals=True)
     if len(sys.argv) == 1:
+        from pyfiglet import Figlet
+        figlet = Figlet(font="doom")
+        console.print(figlet.renderText("PEEP"), style="green", justify="center")
         c_print("No arguments provided, exiting...\nUse peep help for more info", code="danger")
         response = prompt("Type 'help' to get help, press any key to exit")
         if response.lower() == "help":
@@ -18,32 +20,36 @@ def main():
         from service.help.help import Help
         help = Help()
         help.run_help()
+    elif len(sys.argv) == 2 and sys.argv[1] not in get_ops()["args"]:
+        c_print(f"Invalid argument: {sys.argv[1]}", code="danger")
+        c_print("Use peep help for more info", code="info")
     else:
         args = get_args()[0]
-        query = get_args()[1]
-        execute = Execute(args[1], query)
-        do_what = args[0]
-        if do_what in get_ops()["args"]:
-            if do_what == "search":
-                execute.search()
-            if do_what == "editor":
-                execute.editor()
-            if do_what == "md":
-                execute.md()
-            if do_what == "ai":
-                execute.ai()
-            if do_what == "qr":
-                execute.qr()
-            if do_what == "img_qr":
-                execute.img_qr()
-            if do_what == "yt":
-                execute.yt()
-            if do_what == "music":
-                execute.music()
-            if do_what == "setting":
-                execute.setting()
-            if do_what == "file":
-                execute.file()
+        if args[0] in get_ops()["args"]:
+            query = get_args()[1]
+            execute = Execute(args[1], query)
+            do_what = args[0]
+            if do_what in get_ops()["args"]:
+                if do_what == "search":
+                    execute.search()
+                if do_what == "editor":
+                    execute.editor()
+                if do_what == "md":
+                    execute.md()
+                if do_what == "ai":
+                    execute.ai()
+                if do_what == "qr":
+                    execute.qr()
+                if do_what == "img_qr":
+                    execute.img_qr()
+                if do_what == "yt":
+                    execute.yt()
+                if do_what == "music":
+                    execute.music()
+                if do_what == "setting":
+                    execute.setting()
+                if do_what == "file":
+                    execute.file()
 
 if __name__ == '__main__':
     main()
--- src/service/help/help.py
+++ src/service/help/help.py
@@ -1,9 +1,20 @@
-import os
+import os, json
 from rich.console import Console
 from rich.panel import Panel
 
 console = Console()
 
+does = {
+    "search": "Searches the web for the query",
+    "editor": "Opens the web based text editor",
+    "md": "Converts the query to markdown",
+    "ai": "Opens the AI",
+    "qr": "Converts the query to QR Code",
+    "img_qr": "Converts the image to QR Code",
+    "yt": "Searches YouTube for the query and streams the video",
+    "music": "Search music and play in the custom player",
+    "setting": "Opens the Settings",
+}
 
 class Help:
     def __init__(self):
@@ -19,6 +30,17 @@ class Help:
                       "PEEP lives in the command line and loves it there. And it's here to help you with"
                       " your daily tasks. PEEP is not perfect, but it's getting there. PEEP is a work in progress."
                       " PEEP is developed by [link=https://noobscience.rocks]NoobScience[/link] and is licensed under the MIT License. ")
-
+        console.print("Available commands:", style="bold red underline")
+        from rich import print_json
+        print_json(data=json.loads(str(does).replace("'", '"')))
+        console.print("It is easier to represent PEEP commands in a HTML file.", style="bold")
+        from ..format import ask
+        from ..brain import platform
+        internet_response = ask("Are you connected to the internet?:")
+        if internet_response:
+            if platform() == "win":
+                os.system("start https://peep.noobscience.rocks")
+            else:
+                os.system("open https://peep.noobscience.rocks")
     def run_help(self):
         self.help_content()
